4422bf14824b1a52195d594079b53590095ddbb9
Biancamorinatt <biancamorinatt@gmail.com>
2018-06-03 19:06:38 -0300

    Update share.php

--- share.php
+++ share.php
@@ -6,13 +6,13 @@ $highscore = $scorex[0];
 ?>
 <html>
 <head>
-<title>Weebe Jello Jumper HighScore</title>
+<title>score facebook desenvolvido pela heart soft studio</title>
 <meta property="og:title" content="Highscore on Game is <?php echo $highscore;?>!"/>
-<meta property="og:image" content="http://yoursite.com/game_icon.png"/>
+<meta property="og:image" content="http://dactari.com.br/game_icon.png"/>
 <meta property="og:site_name" content="Your company"/>
 <meta property="og:description" content="<?php echo $highscore;?> is mine new highscore on Game! How far do you think you can get?"/>	
 </head>
 <body>
-<meta http-equiv="refresh" content="0;URL=http://www.yoursite.com" />
+<meta http-equiv="refresh" content="0;URL=http://www.dactari.com.br" />
 </body>
 </html>
