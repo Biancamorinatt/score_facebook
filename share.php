524a63e9194e28a523fe2f61657fc0b0fea2f91d
Biancamorinatt <biancamorinatt@gmail.com>
2018-06-03 19:08:03 -0300

    Update share.php

--- share.php
+++ share.php
@@ -10,7 +10,7 @@ $highscore = $scorex[0];
 <meta property="og:title" content="Highscore on Game is <?php echo $highscore;?>!"/>
 <meta property="og:image" content="http://dactari.com.br/game_icon.png"/>
 <meta property="og:site_name" content="Your company"/>
-<meta property="og:description" content="<?php echo $highscore;?> is mine new highscore on Game! How far do you think you can get?"/>	
+<meta property="og:description" content="<?php echo $highscore;?> é meu novo recorde no jogo! Até onde você acha que consegue chegar?"/>	
 </head>
 <body>
 <meta http-equiv="refresh" content="0;URL=http://www.dactari.com.br" />
